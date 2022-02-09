143cfe007e8daf28fdfab4ba0d413d0048819b2e
howshekilledit <cellardoor.morgan@gmail.com>
2022-02-09 10:19:40 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+let clrs = {
+  top:'yellow',
+  m1: 'red',
+  m2: 'cyan',
+  bottom: 'violet',
+  strip: 'pink',
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  //top
+  fillRect(0, height/2, clrs.top);
+  //bottom
+  fillRect(height/2, height/2, clrs.bottom);
+  //center strip
+  strip();
+  //top middle
+  fillRect(height/3, height/6, clrs.m1); 
+  //bottom middle
+  fillRect(height/2, height/6, clrs.m2); 
+  if(mouseIsPressed){
+      //top
+    fillRect(0, height/2, clrs.top);
+    //bottom
+    fillRect(height/2, height/2, clrs.bottom);
+    strip(); 
+    
+  }
+}
+
+function fillRect(tp, hght, clr){
+  fill(clr);
+  noStroke();
+  rect(0, tp, width, hght);
+}
+
+function strip(){
+  //center
+  fill(clrs.strip);
+  rect(width/3, height/6, width/3, height * 2/3);
+  
+}
